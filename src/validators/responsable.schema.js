9dd82a3f9be2c02a717ee26196732d0d17922b04
Felipe Becerra <felipe.e.becerra@gmail.com>
2026-01-11 22:38:39 -0300

    fixed responsable validator

--- src/validators/responsable.schema.js
+++ src/validators/responsable.schema.js
@@ -31,7 +31,18 @@ export const responsableSchemaCreate = z.object({
         .trim()
         .min(9, "El rut no puede estar vacío")
         .max(12, "El rut no puede tener más de 12 caracteres")
-        .regex(/^[0-9]{1,12}[-|\s]{1}[0-9K]{1,3}$/, "El rut debe tener el formato 12345678-9")
+        .regex(/^[0-9]{1,12}[-|\s]{1}[0-9K]{1,3}$/, "El rut debe tener el formato 12345678-9"),
+
+    estado: z.string({
+        required_error: "El estado es obligatorio",
+        invalid_type_error: "Ingresar un estado válido"
+    })
+        .trim()
+        .min(1, "El estado no puede estar vacío")
+        .max(50, "El estado no puede tener más de 50 caracteres")
+        .transform((value) => {
+            return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
+        })
 });
 
 export const responsableSchemaUpdate = responsableSchemaCreate.partial();
